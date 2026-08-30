d16961f7bf2844f2672dafb0f7035f066f0f6844
Derok <derekrojo@gmail.com>
2026-08-30 14:48:18 -0400

    correccion de bug

---
+++ temp_0.js
@@ -0,0 +1,29 @@
+
+      tailwind.config = {
+        darkMode: "class",
+        theme: {
+          extend: {
+            colors: {
+              crema: "#FFE1D7",
+              azulcielo: {
+                DEFAULT: "#84AFFB",
+                light: "#b3cdfc",
+                dark: "#5a90e8",
+              },
+              azulelectrico: {
+                DEFAULT: "#0259DD",
+                hover: "#0249b5",
+                dark: "#01388a",
+                light: "#3b82f6",
+              },
+              coral: {
+                DEFAULT: "#FF6648",
+                hover: "#e05337",
+                dark: "#b83b22",
+                light: "#ff8a73",
+              },
+            },
+          },
+        },
+      };
+    
